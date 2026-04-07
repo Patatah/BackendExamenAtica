0b0473c9cf0ce7ab23119934b0788808f960b843
Edgar Alán Fernández Covarrubias <edgar1mas2@gmail.com>
2026-04-07 12:17:42 -0700

    Dtos create y patch

--- src/productos/dto/create-producto.dto.ts
+++ src/productos/dto/create-producto.dto.ts
@@ -1,1 +1,36 @@
-export class CreateProductoDto {}
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength } from "class-validator";
+
+// https://github.com/typestack/class-validator#validation-decorators
+// https://github.com/typestack/class-transformer/blob/develop/sample/sample1-simple-usage/Photo.ts
+
+export class CreateProductoDto {
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(255)
+    nombre: string;
+
+    @IsNotEmpty()
+    @IsString()
+    urlFoto: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(4000)
+    descripcion?: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Type(() => Number)
+    precio: number;
+
+    @IsOptional()
+    @IsNumber()
+    @Type(() => Number)
+    stock?: number;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(100)
+    categoria?: string;
+}
--- src/productos/dto/update-producto.dto.ts
+++ src/productos/dto/update-producto.dto.ts
@@ -1,4 +1,33 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductoDto } from './create-producto.dto';
+import { IsOptional, IsString, MaxLength, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateProductoDto extends PartialType(CreateProductoDto) {}
+export class UpdateProductoDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  nombre?: string;
+
+  @IsOptional()
+  @IsString()
+  urlFoto?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(4000)
+  descripcion?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  precio?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  stock?: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(100)
+  categoria?: string;
+}
